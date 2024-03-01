13f001a2a8a9b004441f169e75adb1de91aa314d
imsunnythecode <161055137+imsunnythecode@users.noreply.github.com>
2024-03-01 15:02:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -274,3 +274,5 @@ function cumulativeDistribution(x) {
 function probabilityDensity(x) {
   return Math.exp(-x * x / 2) / Math.sqrt(2 * Math.PI);
 }
+
+document.getElementById("expirySelect").addEventListener("change", main)
